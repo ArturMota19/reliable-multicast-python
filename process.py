7e4658b15f234ec1c1368592cc53feedd900e2fb
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-06-30 10:57:13 -0300

    INITIAL: Add imports and main function

---
+++ process.py
@@ -0,0 +1,13 @@
+import socket
+import threading
+import json
+import time
+
+# Disseminação de mensagens para um grupo de processos distribuídos, de modo que
+# todos os processos corretos recebam o mesmo conjunto de mensagens, mesmo na
+# ocorrência de falhas de parada (crash) de processos do grupo
+
+if __name__ == "__main__":
+  print("Rodando")
+
+
