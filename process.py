b62a76a397ebf01d43570617ff94409ec66ae3f5
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-06-30 11:05:59 -0300

    FEAT: Add listener to process

--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import json
 import time
+import sys
 
 # Disseminação de mensagens para um grupo de processos distribuídos, de modo que
 # todos os processos corretos recebam o mesmo conjunto de mensagens, mesmo na
@@ -38,7 +39,22 @@ class Process:
         for peer in self.peers: # enviando a mensagem para peers
             self.sock.sendto(message, ('localhost', peer))
             
+    def listen(self):
+        while True:
+            data, addr = self.sock.recvfrom(1024) # recebendo a mensagem
+            message = json.loads(data.decode()) # decodificando a mensagem recebida
+            self.clock.update(message['time'])  # atualizando o relógio Lamport
+            print(f"O processo {self.pid} recebeu a msg: {message['msg']} de {addr[1]} no tempo {self.clock.time}")
+            
 if __name__ == "__main__":
   print("Rodando")
+  peers = [('localhost', 10001), ('localhost', 10002), ('localhost', 10003)]
+  print(sys.argv)
+  idx = int(sys.argv[1])
+  process = Process(idx + 1, peers[:idx] + peers[idx+1:], peers[idx][1])
+
+  while True:
+      msg = input(f"[P{idx+1}] Digite uma mensagem: ")
+      process.send(msg)
 
 
