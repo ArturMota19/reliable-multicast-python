fa9b9b59dd13549506c1f8fbf811c4dd9e77829d
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-06-30 11:01:25 -0300

    FEAT: Add send function

--- process.py
+++ process.py
@@ -7,6 +7,7 @@ import time
 # todos os processos corretos recebam o mesmo conjunto de mensagens, mesmo na
 # ocorrência de falhas de parada (crash) de processos do grupo
 
+# O lamport clock serve para sincronizar os processos distribuídos
 class LamportClock:
     def __init__(self):
         self.time = 0
@@ -16,7 +17,8 @@ class LamportClock:
 
     def update(self, received_time):
         self.time = max(self.time, received_time) + 1
-        
+
+# Crio o processo que vai receber e enviar as mensagens        
 class Process:
     def __init__(self, pid, peers, port):
         self.pid = pid
@@ -25,7 +27,17 @@ class Process:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('localhost', port))
         threading.Thread(target=self.listen).start()
-
+        
+    def send(self, msg):
+        self.clock.tick() # tickando o tempo do processo
+        message = json.dumps({ # criando o objeto da mensagem que vou enviar
+            'pid': self.pid,
+            'time': self.clock.time,
+            'msg': msg
+        }).encode()
+        for peer in self.peers: # enviando a mensagem para peers
+            self.sock.sendto(message, ('localhost', peer))
+            
 if __name__ == "__main__":
   print("Rodando")
 
